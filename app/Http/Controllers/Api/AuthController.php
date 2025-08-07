a1f6ebd91a25611d0649a2601e0f5eccdcd23877
gilangcahyono <gilcah2207@gmail.com>
2025-08-07 22:33:33 +0700

    home: 070820252233

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Error;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -14,29 +15,87 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
         $data = $request->validated();
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        return response()->json([
+            'ok' => true,
+            'message' => 'User logged in successfully',
+            'data' => $user,
+            'errors' => null
+        ]);
     }
 
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request) // done
     {
         $data = $request->validated();
-        $user = User::where('email', $data['email'])->first();
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        try {
+            $user = User::where('email', $data['email'])->first();
+
+            if (!$user) {
+                return response()->json([
+                    'ok' => false,
+                    'message' => 'Invalid credentials',
+                    'data' => null,
+                    'errors' => [
+                        'email' => ['Email not found']
+                    ]
+
+                ], 401);
+            }
+
+            if (!Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'ok' => false,
+                    'message' => 'Invalid credentials',
+                    'data' => null,
+                    'errors' => [
+                        'password' => ['Incorrect password']
+                    ]
+
+                ], 401);
+            }
+
+            // if (true) throw new Error();
+
+            $token =  $user->createToken('auth_token')->plainTextToken;
+
             return response()->json([
-                'message' => 'Username or password is wrong'
-            ], 401);
+                'ok' => true,
+                'message' => 'User logged in successfully',
+                'data' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'token' => $token
+                ],
+                'errors' => null
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'ok' => false,
+                'message' => 'Internal server error',
+                'data' => null,
+                'errors' => $th->getMessage()
+            ], 500);
         }
-        $token =  $user->createToken('auth_token')->plainTextToken;
-        return response()->json(['token' => $token]);
     }
 
     public function user(Request $request)
     {
-        return $request->user();
+        $user = [
+            'id' => $request->user()->id,
+            'name' => $request->user()->name,
+            'email' => $request->user()->email
+        ];
+        return response()->json([
+            'ok' => true,
+            'message' => 'Current user',
+            'data' => $user,
+            'errors' => null
+        ]);
     }
 
     public function logout(Request $request)
